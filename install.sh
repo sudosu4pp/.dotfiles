aa7e46a21ddc9f73ea8a8835b2b0724842979d9c
4ctor <129112308+sudosu4pp@users.noreply.github.com>
2023-10-22 00:57:25 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,24 +1,18 @@
-# install nix
-curl -L https://nixos.org/nix/install | sh
-
-# source nix
-. ~/.nix-profile/etc/profile.d/nix.sh
-
 # install packages
 nix-env -iA \
-	nixpkgs.zsh \
-	nixpkgs.antibody \
-	nixpkgs.git \
-	nixpkgs.neovim \
-	nixpkgs.tmux \
-	nixpkgs.stow \
-	nixpkgs.yarn \
-	nixpkgs.fzf \
-	nixpkgs.ripgrep \
-	nixpkgs.bat \
-	nixpkgs.gnumake \
-	nixpkgs.gcc \
-	nixpkgs.direnv
+	nixos.zsh \
+	nixos.antibody \
+	nixos.git \
+	nixos.neovim \
+	nixos.tmux \
+	nixos.stow \
+	nixos.yarn \
+	nixos.fzf \
+	nixos.ripgrep \
+	nixos.bat \
+	nixos.gnumake \
+	nixos.gcc \
+	nixos.direnv
 
 # stow dotfiles
 stow git
